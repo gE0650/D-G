66ebca0e2063ce1109ba43e04affd02d67c13b0f
gE0650 <2500013036@stu.pku.edu.cn>
2025-10-19 16:36:01 +0800

    finished frontend&backend cowork

--- frontend/scripts/tasksaction.js
+++ frontend/scripts/tasksaction.js
@@ -2,33 +2,49 @@ const template = document.getElementById("taskbartemplate");
 const Container = document.getElementById("tasksblock");
 const input = document.getElementById("input");
 const API_URL = "http://localhost:3000/todos";
+try {
+  const res = await fetch(API_URL, { method: "GET" });
+  const todos = await res.json();
+  todos.forEach((todo) => addTaskToDOM(todo));
+} catch (err) {
+  console.error("数据加载错误", err);
+}
+
+function addTaskToDOM(todo) {
+  const clone = template.content.cloneNode(true);
+  const taskbar = clone.querySelector(".taskbar");
+  taskbar.querySelector(".tasktext").textContent = todo.text;
+  Container.appendChild(taskbar);
+  const checkbox = taskbar.querySelector(".taskcheck");
+  checkbox.checked = false;
+  checkbox.addEventListener("click", async () => {
+    try {
+      await fetch(`${API_URL}/${todo.id}`, {
+        method: "DELETE",
+      });
+      taskbar.remove();
+    } catch (err) {
+      console.error("删除任务失败", err);
+    }
+  });
+}
 
 document.getElementById("buttonadd").addEventListener("click", async () => {
-  const clone = template.content.cloneNode(true);
-
   const text = input.value.trim();
-  const taskbar = clone.querySelector(".taskbar");
   if (!text) {
     alert("任务栏一定要有任务!");
     return;
   }
-
-  const res = await fetch(API_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ text }),
-  });
-  const todo = await res.json();
-  console.log("后端返回:", todo);
-  taskbar.querySelector(".tasktext").textContent = todo.text;
-  Container.appendChild(taskbar);
-  input.value = "";
-  const checkbox = taskbar.querySelector(".taskcheck");
-  checkbox.checked = false;
-  checkbox.addEventListener("click", async () => {
-    await fetch(`${API_URL}/${todo.id}`, {
-      method: "DELETE",
+  try {
+    const res = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text }),
     });
-    taskbar.remove();
-  });
+    const todo = await res.json();
+    addTaskToDOM(todo);
+    input.value = "";
+  } catch (err) {
+    console.log("增加任务出错喵", err);
+  }
 });
